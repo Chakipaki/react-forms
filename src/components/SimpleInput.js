329a7cd3da7da7184c38b30c4f0a8af38f48f667
Ivan Bieliaiev <belyaeve2@icloud.com>
2023-06-02 21:29:38 +0200

    Forms

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,15 +1,53 @@
+import {useRef, useState} from "react";
+
 const SimpleInput = (props) => {
-  return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
-      </div>
-      <div className="form-actions">
-        <button>Submit</button>
-      </div>
-    </form>
-  );
+    const nameRef = useRef();
+    const [enteredName, setEnteredName] = useState('');
+    const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
+    const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+
+    const nameChangeHandler = (event) => {
+        setEnteredName(event.target.value);
+    }
+
+    const nameIsInvalid = !enteredNameIsValid && enteredNameTouched;
+
+    const formSubmissionHandler = (event) => {
+        event.preventDefault();
+
+        setEnteredNameTouched(true);
+
+        if (!enteredName.trim().length) {
+            setEnteredNameIsValid(false);
+            return;
+        }
+
+        setEnteredNameIsValid(true);
+
+        console.log(enteredName);
+        setEnteredName('');
+    }
+
+    const inputClasses = 'form-control ' + (nameIsInvalid ? 'invalid' : '');
+
+    return (
+        <form onSubmit={formSubmissionHandler}>
+            <div className={inputClasses}>
+                <label htmlFor='name'>Your Name</label>
+                <input
+                    ref={nameRef}
+                    type='text'
+                    id='name'
+                    value={enteredName}
+                    onChange={nameChangeHandler}
+                />
+                {nameIsInvalid && <p className="error-text">Entered Name can't be empty!</p>}
+            </div>
+            <div className="form-actions">
+                <button>Submit</button>
+            </div>
+        </form>
+    );
 };
 
 export default SimpleInput;
